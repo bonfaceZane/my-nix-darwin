1e9082784f76dd8de0c44eb4ebee30a11ddbc1ed
Bonface Zane <obwoni000@nlm72hy6qnjl.atlantis.local>
2024-12-19 16:20:22 +0100

    added sh update files

---
+++ scripts/setup.sh
@@ -0,0 +1,17 @@
+
+# run curl to download the latest nix package
+curl \
+  --proto '=https' \
+  --tlsv1.2 \
+  -sSf \
+  -L https://install.determinate.systems/nix \
+  | sh -s -- install
+
+
+#   run script to install nix-darwin
+curl \
+  --proto '=https' \
+  --tlsv1.2 \
+  -sSf \
+  -L https://install.determinate.systems/nix-darwin \
+  | sh -s -- install
